5a292e98c79e7cdd16394100750745476c0c365a
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-01 22:14:55 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   smallest_hash_key = nil
-  name_hash.each do |key, val|
+  name_hash.map do |key, val|
     smallest_hash_key ||= key
     smallest_hash_key = key if val < name_hash[smallest_hash_key]
   end
