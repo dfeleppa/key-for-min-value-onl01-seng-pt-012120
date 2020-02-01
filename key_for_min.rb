5f10a5335fac05eec2df609009574327176abcb7
Daniel J Feleppa <dfeleppa@gmail.com>
2020-02-01 22:16:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,10 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest_hash_key = nil
+  smallest_val = nil
   name_hash.map do |key, val|
     smallest_hash_key ||= key
     smallest_hash_key = key if val < name_hash[smallest_hash_key]
   end
-  smallest_hash_key
+  smallest_val
 end
